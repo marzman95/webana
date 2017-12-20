d4847f9393dca869b2bc56bef9e9d108c9f8e5c6
marzman95 <marcinvandeven@gmail.com>
2017-12-20 12:02:58 +0100

    Example for classes

---
+++ pseudo.py
@@ -0,0 +1,6 @@
+class MyClass:
+    def __init__(self, num):
+        self.count = num+1
+
+    def cool(self):
+        print(self.count)
---
+++ test.py
@@ -0,0 +1,3 @@
+from pseudo import MyClass;
+hello = MyClass(5)
+hello.cool()
